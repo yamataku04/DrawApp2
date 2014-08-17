dc3e7d2b7e76d5aa96f23981f5a3cdd9eecc30d8
yamadatakumi <takumi.yamada.0419@gmail.com>
2014-08-17 14:06:56 +0900

    new

--- src/com/example/drawapp2/MainActivity.java
+++ src/com/example/drawapp2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.drawapp2;
 
+import org.w3c.dom.Text;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -9,29 +11,55 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-       Context context;
+       
+	Context context;
+     TextView textView;
+     EditText editText;
+     
+     
+     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		context=this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button button=(Button) findViewById(R.id.button1);
-		button.setOnClickListener(new OnClickListener(){//タッチされるまで待機している
+		Button button1=(Button) findViewById(R.id.button1);
+
+		TextView textview1=(TextView) findViewById(R.id.textView1);
+		
+		
+		
+		button1.setOnClickListener(new OnClickListener(){//タッチされるまで待機している
 			
 			@Override
 			public void onClick(View v){
 				Toast.makeText(context, "Touchされました",Toast.LENGTH_SHORT).show();
 			}
 		});
-		Log.v("TAG","verbose");
+		/**Log.v("TAG","verbose");
 		Log.d("TAG","verbose");
 		Log.i("TAG","verbose");
 		Log.w("TAG","verbose");
-		Log.e("TAG","verbose");
+		Log.e("TAG","verbose");**/
+		textView=(TextView)findViewById(R.id.textView2);
+		editText=(EditText)findViewById(R.id.editText1);
+		Button button2=(Button) findViewById(R.id.button2);
+		button2.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				String text = editText.getText().toString();
+				textView.setText(text);
+			
+			}
+		});
+	
+		
 		
 	}
 
